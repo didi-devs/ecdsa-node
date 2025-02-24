2207c78b823bd8144ac83eb4459bbb039541aed0
didi-devs <oladejofavourajoke@gmail.com>
2025-02-24 23:49:20 +0100

    ...

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,8 +1,12 @@
 import server from "./server";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+import * secp from 'ethereum-cryptography/secp256k1';
+
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey); 
+    const address = secp.getPublicKey(privateKey);
     setAddress(address);
     if (address) {
       const {
@@ -19,8 +23,8 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+         Private Key
+        <input placeholder="Type a Private Key" value={address} onChange={onChange}></input>
       </label>
 
       <div className="balance">Balance: {balance}</div>
